d42a1c28b820922f960223ba7c86dfb774cb70d3
ri.gayfullin <ri.gayfullin@tensor.ru>
2023-10-10 12:21:14 +0300

    Initial Commit

---
+++ task4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  task4
+//
+//  Created by Руслан Гайфуллин on 10.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ task4/task4App.swift
@@ -0,0 +1,17 @@
+//
+//  task4App.swift
+//  task4
+//
+//  Created by Руслан Гайфуллин on 10.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct task4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
